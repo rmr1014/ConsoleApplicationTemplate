1639541d4b2a334c47ad89be05e5008c270103f9
Ryan Ross <rmr1014@gmail.com>
2019-11-13 15:02:43 -0500

    Initial Commit (#1)

---
+++ ConsoleApplicationTemplate/ConsoleCommand.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplicationTemplate
+{
+    public class ConsoleCommand
+    {
+        public ConsoleCommand(string input)
+        {
+            var stringArray = Regex.Split(input, "(?<=^[^\"]*(?:\"[^\"]*\"[^\"]*)*) (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            _arguments = new List<string>();
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (i == 0)
+                {
+                    this.Name = stringArray[i];
+                    this.LibraryClassName = "DefaultCommands";
+                    string[] s = stringArray[0].Split('.');
+
+                    if (s.Length == 2)
+                    {
+                        this.LibraryClassName = s[0];
+                        this.Name = s[1];
+                    }
+                }
+                else
+                {
+                    var inputArgument = stringArray[i];
+                    string argument = inputArgument;
+                    var regex = new Regex("\"(.*?)\"", RegexOptions.Singleline);
+                    var match = regex.Match(inputArgument);
+
+                    if (match.Captures.Count > 0)
+                    {
+                        var captureQuotedText = new Regex("[^\"]*[^\"]");
+                        var quoted = captureQuotedText.Match(match.Captures[0].Value);
+                        argument = quoted.Captures[0].Value;
+                    }
+                    _arguments.Add(argument);
+                }
+            }
+        }
+
+        public object Name { get; }
+        public string LibraryClassName { get; }
+
+        private readonly List<string> _arguments;
+
+        public IEnumerable<string> Arguments
+        {
+            get
+            {
+                return _arguments;
+            }
+        }
+    }
+}
---
+++ ConsoleApplicationTemplate/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApplicationTemplate
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = typeof(Program).Name;
+
+            Run();
+        }
+
+        private static void Run()
+        {
+            while (true)
+            {
+                var consoleInput = ReadFromConsole();
+
+                if (string.IsNullOrWhiteSpace(consoleInput))
+                    continue;
+
+                try
+                {
+                    string result = Execute(consoleInput);
+
+                    WriteToConsole(result);
+                }
+                catch (Exception ex)
+                {
+                    WriteToConsole(ex.Message);
+                }
+            }
+        }
+
+        public static void WriteToConsole(string message = "")
+        {
+            if (message.Length > 0)
+                Console.WriteLine(message);
+        }
+
+        private static string Execute(string command)
+        {
+            return string.Format($"Executed the {command} Command");
+        }
+
+        private const string _readPrompt = "console> ";
+
+        public static string ReadFromConsole(string promptMessage = "")
+        {
+            Console.WriteLine(_readPrompt + promptMessage);
+
+            return Console.ReadLine();
+        }
+    }
+}
