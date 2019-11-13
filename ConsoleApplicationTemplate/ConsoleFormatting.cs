c479229927c7074d6d1697073b817acbf96453dc
Ryan Ross <rmr1014@gmail.com>
2019-11-13 15:37:11 -0500

    Updated Run and Execute methods to use ConsoleCommand (#2)

---
+++ ConsoleApplicationTemplate/ConsoleFormatting.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApplicationBase
+{
+    public static class ConsoleFormatting
+    {
+        public static string Indent(int count)
+        {
+            return "".PadLeft(count);
+        }
+    }
+}
