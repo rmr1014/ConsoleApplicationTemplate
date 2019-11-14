ecac141baf1da31b4420ce13ed640b2f273f35ab
Ross, Ryan <ryanross@amrock.com>
2019-11-14 12:25:37 -0500

    Added Data Models and Business Logic

---
+++ ConsoleApplicationTemplate/Commands/Users.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApplicationBase.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationBase.Commands
+{
+    public static class Users
+    {
+        public static string Create(string firstName, string lastName)
+        {
+            int? maxId = SampleData.Users.Max(u => u.Id);
+
+            int newId = 0;
+
+            if (maxId.HasValue)
+                newId = maxId.Value + 1;
+
+            var newUser = new User
+            {
+                Id = newId,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            SampleData.Users.Add(newUser);
+
+            return "";
+        }
+
+        public static string Get()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var user in SampleData.Users)
+            {
+                sb.Append(ConsoleFormatting.Indent(2)).AppendFormat($"Id:{user.Id} {user.FirstName} {user.LastName}").AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
