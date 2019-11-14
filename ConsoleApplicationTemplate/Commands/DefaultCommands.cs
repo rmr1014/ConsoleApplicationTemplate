7ceb5afa1a830b002fe2142c9127947b416949e1
Ryan Ross <rmr1014@gmail.com>
2019-11-14 10:33:50 -0500

    Added default commands class (#3)

---
+++ ConsoleApplicationTemplate/Commands/DefaultCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplicationBase.Commands
+{
+    public static class DefaultCommands
+    {
+        public static string DoSomething(int id, string data)
+        {
+            return string.Format($"I did something to the record Id {id} and save the data {data}");
+        }
+
+        public static string DoSomethingElse(DateTime date)
+        {
+            return string.Format($"I did something else on {date}");
+        }
+
+        public static string DOSomethingOptional(int id, string data = "No Data provided")
+        {
+            var result = string.Format($"I did something to the record Id {id} and save the data '{data}'");
+
+            if (data == "No Data provided")
+                result = string.Format($"I did something to the record Id {id} but the optional parameter was not provided, so I saved the value '{data}'");
+
+            return result;
+        }
+    }
+}
